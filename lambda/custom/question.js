a44cd06ea9fe4c146cf5040f52775b18eb8d5ad3
Josh Gormley <jgormley@Joshs-MacBook-Pro.local>
2018-01-07 19:11:13 -0600

    initial add

---
+++ lambda/custom/question.js
@@ -0,0 +1,71 @@
+'use strict';
+
+module.exports = {
+    /**
+     * When editing your questions pay attention to your punctuation. Make sure you use question marks or periods.
+     * Make sure the first answer is the correct one. Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+     */
+    QUESTIONS_EN_US: [
+        {
+            'Reindeer have very thick coats, how many hairs per square inch do they have?': [
+                '13,000',
+                '1,200',
+                '5,000',
+                '700',
+                '1,000',
+                '120,000',
+            ],
+        },
+        {
+            'The 1964 classic Rudolph The Red Nosed Reindeer was filmed in. ': [
+                'Japan',
+                'United States',
+                'Finland',
+                'Germany',
+                'Canada',
+                'Norway',
+                'France',
+            ],
+        },
+        {
+            'Santas reindeer are cared for by one of the Christmas elves, what is his name?': [
+                'Wunorse Openslae',
+                'Alabaster Snowball',
+                'Bushy Evergreen',
+                'Pepper Minstix',
+            ],
+        },
+        {
+            'If all of Santas reindeer had antlers while pulling his Christmas sleigh, they would all be': [
+                'Girls',
+                'Boys',
+                'Girls and boys',
+                'No way to tell',
+            ],
+        },
+        {
+            'What do Reindeer eat?': [
+                'Lichen',
+                'Grasses',
+                'Leaves',
+                'Berries',
+            ],
+        },
+        {
+            'What of the following is not true?': [
+                'Caribou live on all continents',
+                'Both reindeer and Caribou are the same species',
+                'Caribou are bigger than reindeer',
+                'Reindeer live in Scandinavia and Russia',
+            ],
+        },
+        {
+            'In what year did Rudolph make his television debut?': [
+                '1964',
+                '1979',
+                '2000',
+                '1956',
+            ],
+        },
+    ],
+};
